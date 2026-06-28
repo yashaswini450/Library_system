bd8ef2f45ef1cacf6bb83c2e01636fd6d14d81a4
yashaswini450 <yashaswinikalpataru@gmail.com>
2026-06-28 19:21:37 +0530

    Docker: add server/client Dockerfiles, compose, and configurable SERVER_HOST

--- cli_files/cli.c
+++ cli_files/cli.c
@@ -21,7 +21,9 @@ int main() {
 
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    const char *host = getenv("SERVER_HOST");
+    if (!host) host = "127.0.0.1";
+    server_address.sin_addr.s_addr = inet_addr(host);
 
     if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
         perror("Connection error");
